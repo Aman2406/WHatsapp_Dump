66b5d188ae77526eb2ae28e7ee9d24428b542846
AnwarMohamed <anwarelmakrahy@gmail.com>
2017-03-17 14:53:00 +0200

    adding initial auxiliary skeleton

---
+++ modules/auxiliary/gather/ss7_send_routing_info_for_sm.rb
@@ -0,0 +1,34 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Auxiliary
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'          => 'SS7 SendRoutingInfoForSM',
+      'Description'   => %q{
+
+      },
+      'Author'        => [
+          'Anwar Mohamed <anwarelmakrahy[at]gmail.com>',
+          'Loay Abd ElRazek <loay.razek[at]gmail.com>'
+      ],
+      'License'       => MSF_LICENSE
+    ))
+
+    register_options(
+      [
+        OptString.new('StrKey', [ false, 'Description', 'Default' ]),
+        OptBool.new('BoolKey', [ false, 'Description', false ]),
+        OptInt.new('IntKey', [ true, 'Description', 300 ])
+      ], self.class)
+  end
+
+  def run
+    print_status("Starting SS7 Auxiliary")
+  end
+end
